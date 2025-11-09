1706170e91fc98cee3102fe47b2c3ec71239490b
sailormews <190859321+sailormews@users.noreply.github.com>
2025-11-09 15:44:24 -0700

    created a function that randomly returns rock paper or scissors

--- myscript.js
+++ myscript.js
@@ -1,1 +1,12 @@
 console.log("Hello, World!")
+
+function getComputerChoice() {
+    const randomChoice = Math.random(); 
+if (randomChoice < 1/3) { 
+    return "Rock"; 
+} else if (randomChoice < 2/3) { 
+    return "Paper"; 
+} else { 
+    return "Scissors";
+} 
+}
