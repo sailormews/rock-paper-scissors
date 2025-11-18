a1c26a11d44955433318bff665b3d2ab8bea3fe1
sailormews <190859321+sailormews@users.noreply.github.com>
2025-11-17 18:56:09 -0700

    write the logic to get the computer choice and human choice

--- myscript.js
+++ myscript.js
@@ -1,7 +1,7 @@
 console.log("Hello, World!")
 
 function getComputerChoice() {
-    const randomChoice = Math.random(); 
+    let randomChoice = Math.random(); 
 if (randomChoice < 1/3) { 
     return "Rock"; 
 } else if (randomChoice < 2/3) { 
@@ -10,3 +10,33 @@ if (randomChoice < 1/3) {
     return "Scissors";
 } 
 }
+
+function getHumanChoice() {
+    let userChoice = prompt ("Please enter rock paper or scissors:"); 
+    return userChoice;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            console.log("its a tie!"); 
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
+            console.log("you win paper beats rock"); 
+        } else if (humanChoice === "rock" && computerChoice === "scissors") {
+            console.log("you win rock beats scissors"); 
+        } else if (humanChoice === "scissors" && computerChoice === "paper") {
+            console.log("you win scissors beats paper")
+        } else { 
+            console.log("You lose! ${computerChoice} beats ${humanChoice}"); 
+        }
+    } 
+
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+
+
